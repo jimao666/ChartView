3a369ec161d048869d80a542243fca41fe181e10
jimao666 <142883081+jimao666@users.noreply.github.com>
2024-01-07 15:29:00 +0800

    Update ExampleInstrumentedTest.java

--- chartviewLibrary/src/androidTest/java/com/jimao666/chart/ExampleInstrumentedTest.java
+++ chartviewLibrary/src/androidTest/java/com/jimao666/chart/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.lihang.chart;
+package com.jimao666.chart;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.lihang.mycustomview", appContext.getPackageName());
+        assertEquals("com.jimao666.mycustomview", appContext.getPackageName());
     }
 }
